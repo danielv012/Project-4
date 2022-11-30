ee57825468a92e98c3b2603d35c687e49e851b1a
Daniel Vayman <daniel@vayman.co>
2022-11-30 00:11:26 -0600

    Finished logic of hashmap class, not tested at all

--- GenericHashMap.h
+++ GenericHashMap.h
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <fstream>
 
+const double LOAD_FACTOR = 0.75;
+
 using namespace std;
 
 template<typename T>
@@ -28,8 +30,14 @@ public:
     void print();
 
     //Getters
-    int getSize();
-    int getCapacity();
+    int getSize()
+    {
+        return size;
+    }
+    int getItems()
+    {
+        return items;
+    }
 
 
 private:
@@ -42,33 +50,214 @@ private:
 
     vector<Node*> table;
     int size;
-    int capacity;
+    int items;
+
+    bool isPrime(int num);
 };
 
+//-------------Constructors & Destructor----------------//
 template<typename T>
 GenericHashMap<T>::GenericHashMap()
 {
-    //default size
-    int size = 100;
-    //create the vector
-    vector<T> hashTable(size);
+    int size = 11;
+    int items = 0;
+    
+    table.resize(size);
+    for (int i = 0; i < size; i++)
+    {
+        table[i] = nullptr;
+    }
 }
 
-//define the constructor
 template<typename T>
 GenericHashMap<T>::GenericHashMap(int size)
 {
-    //create the vector
-    vector<T> hashTable(size);
+    int items = 0;
+    table.resize(size);
+    for (int i = 0; i < size; i++)
+    {
+        table[i] = nullptr;
+    }
+    this->size = size;
 }
 
-//define the destructor
 template<typename T>
 GenericHashMap<T>::~GenericHashMap()
 {
-    //delete the vector
     delete hashTable;
 }
 
+//-------------Insert & Rehash--------------------------//
+template<typename T>
+void GenericHashMap<T>::insert(string key, T value)
+{
+    int index = hash(key);
+    Node* newNode = new Node;
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = NULL;
+
+    if (table[index] == NULL)
+    {
+        table[index] = newNode;
+    }
+    else
+    {
+        Node* temp = table[index];
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    items++;
+    if ((double)items / size >= LOAD_FACTOR)
+    {
+        rehash();
+    }
+}
+
+//define a rehash function that makes the new size double the old size to the next highest prime number
+template<typename T>
+void GenericHashMap<T>::rehash()
+{
+    int newSize = size * 2;
+    while (!isPrime(newSize))
+    {
+        newSize++;
+    }
+
+    vector<Node*> newTable(newSize);
+
+    for (int i = 0; i < size; i++)
+    {
+        Node* temp = table[i];
+        while (temp != NULL)
+        {
+            int index = hash(temp->key);
+            Node* newNode = new Node;
+            newNode->key = temp->key;
+            newNode->value = temp->value;
+            newNode->next = NULL;
+
+            if (newTable[index] == NULL)
+            {
+                newTable[index] = newNode;
+            }
+            else
+            {
+                Node* temp2 = newTable[index];
+                while (temp2->next != NULL)
+                {
+                    temp2 = temp2->next;
+                }
+                temp2->next = newNode;
+            }
+            temp = temp->next;
+        }
+    }
+    table = newTable;
+    size = newSize;
+}
+
+//-------------Remove----------------------------------//
+template<typename T>
+void GenericHashMap<T>::remove(string key)
+{
+    int index = hash(key);
+    Node* temp = table[index];
+    Node* prev = NULL;
+
+    if (temp != NULL && temp->key == key)
+    {
+        table[index] = temp->next;
+        delete temp;
+        size--;
+        return;
+    }
+
+    while (temp != NULL && temp->key != key)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+    {
+        return;
+    }
+
+    prev->next = temp->next;
+    delete temp;
+    size--;
+}
+
+//-------------Hash-------------------------------------//
+//declare a hash function that uses double hashing for probing
+template<typename T>
+int GenericHashMap<T>::hash(string key)
+{
+    int hash = 0;
+    int index = 0;
+    for (int i = 0; i < key.length(); i++)
+    {
+        hash = hash + (int)key[i];
+    }
+    index = hash % size;
+    return index;
+}
+
+//-------------Get--------------------------------------//
+template<typename T>
+T GenericHashMap<T>::get(string key)
+{
+    int index = hash(key);
+    Node* temp = table[index];
+
+    while (temp != NULL)
+    {
+        if (temp->key == key)
+        {
+            return temp->value;
+        }
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+template<typename T>
+T& GenericHashMap<T>::operator[](string key)
+{
+    int index = hash(key);
+    Node* temp = table[index];
+
+    while (temp != NULL)
+    {
+        if (temp->key == key)
+        {
+            return temp->value;
+        }
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+//-------------Misc--------------------------------------//
+template<typename T>
+bool GenericHashMap<T>::isPrime(int num)
+{
+    if (num == 1)
+    {
+        return false;
+    }
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 #endif //GENERIC_HASH_MAP_H
